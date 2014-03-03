42cb32eb1c32746c7185311c2fc28430c33e9950
Soslan Aldatov <me@supporteam.ru>
2014-03-03 17:05:12 +0700

    a comment was added

--- AsteriskUserPanel/templates/config/users.php
+++ AsteriskUserPanel/templates/config/users.php
@@ -1,5 +1,7 @@
 <?php
 
+//template config
+
 $SCRIPTS = '<link href="css/bootstrap.min.css" rel="stylesheet">
             <link href="css/custom.css" rel="stylesheet">
             <link href="css/x-editable.css" rel="stylesheet">
